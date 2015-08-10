4e72a364c68e11793092242ff19bfe61fb8f5bd4
Louis Geuten <louisgeuten@gmail.com>
2015-08-10 22:29:18 +0200

    Updated sql definitions

--- nebuleuse.sql
+++ nebuleuse.sql
@@ -12,11 +12,12 @@ CREATE TABLE IF NOT EXISTS `neb_achievements` (
   `name` varchar(255) NOT NULL,
   `max` int(10) unsigned NOT NULL,
   `fullName` varchar(255) NOT NULL,
-  `fullDesc` varchar(255) NOT NULL
+  `fullDesc` varchar(255) NOT NULL,
+  `icon` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-INSERT INTO `neb_achievements` (`id`, `name`, `max`, `fullName`, `fullDesc`) VALUES
-(1, 'test', 10, 'test', 'test');
+INSERT INTO `neb_achievements` (`id`, `name`, `max`, `fullName`, `fullDesc`, `icon`) VALUES
+(1, 'test', 24, 'test', 'test', 'http://i.imgur.com/oyrwt3a.png');
 
 CREATE TABLE IF NOT EXISTS `neb_config` (
   `name` varchar(255) NOT NULL,
@@ -27,16 +28,11 @@ INSERT INTO `neb_config` (`name`, `value`) VALUES
 ('maintenance', '0'),
 ('maintenanceMessage', 'Main server is undergoing maintenance.'),
 ('gameVersion', '1'),
-('subMessage', ''),
-('sessionTimeout', '1800'),
-('gameKey', ''),
+('sessionTimeout', '120'),
 ('gameName', ''),
 ('updaterVersion', '1'),
 ('autoRegister', 'true'),
-('defaultAvatar', 'http://i.imgur.com/oyrwt3a.png'),
-('latestCommit', '1ea7b265ac3c6318aaab112528b95dc4d4afb799'),
-('productionBranch', 'master'),
-('updateSystem', 'GitPatch');
+('defaultAvatar', 'http://i.imgur.com/oyrwt3a.png');
 
 CREATE TABLE IF NOT EXISTS `neb_mirrors` (
 `id` smallint(5) unsigned NOT NULL,
@@ -58,7 +54,8 @@ CREATE TABLE IF NOT EXISTS `neb_stats_tables` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `neb_stats_tables` (`tableName`, `fields`, `autoCount`) VALUES
-('kills', 'userid,x,y,z,weapon,map', 1);
+('users', '[{"Name":"timeplayed","Type":"int","Size":11}]', 0),
+('kills', '[{"Name":"userid","Type":"int","Size":11},{"Name":"x","Type":"int","Size":11},{"Name":"y","Type":"int","Size":11},{"Name":"z","Type":"int","Size":11},{"Name":"map","Type":"string","Size":255},{"Name":"weapon","Type":"string","Size":255}]', 1);
 
 CREATE TABLE IF NOT EXISTS `neb_updates` (
   `version` int(11) NOT NULL,
@@ -75,10 +72,10 @@ CREATE TABLE IF NOT EXISTS `neb_users` (
   `rank` tinyint(3) NOT NULL,
   `avatars` varchar(255) NOT NULL,
   `hash` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 INSERT INTO `neb_users` (`id`, `username`, `password`, `rank`, `avatars`, `hash`) VALUES
-(1, 'test', 'q4F_1BnvOQERMAtuwNHoocjO6DiHvt15ol2krqZ60v-NW-tb0_IooASPuZq6iv1tjjT60JIIhA1MZvjTcGhDqA==', 1, '', 'O-z_gcTHzvgoM3ndhFnKVbM-tUcnGZDz_o6mhkWFiL0VTnvCvHFVOBYnvBp23pbz1ZIafCoH_JO51gXlVkmf8w==');
+(1, 'test', 'q4F_1BnvOQERMAtuwNHoocjO6DiHvt15ol2krqZ60v-NW-tb0_IooASPuZq6iv1tjjT60JIIhA1MZvjTcGhDqA==', 2, '', 'O-z_gcTHzvgoM3ndhFnKVbM-tUcnGZDz_o6mhkWFiL0VTnvCvHFVOBYnvBp23pbz1ZIafCoH_JO51gXlVkmf8w==');
 
 CREATE TABLE IF NOT EXISTS `neb_users_achievements` (
   `userid` int(10) unsigned NOT NULL,
@@ -86,20 +83,26 @@ CREATE TABLE IF NOT EXISTS `neb_users_achievements` (
   `progress` int(10) unsigned NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+INSERT INTO `neb_users_achievements` (`userid`, `achievementid`, `progress`) VALUES
+(2, 1, 24);
+
 CREATE TABLE IF NOT EXISTS `neb_users_stats` (
   `userid` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `value` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `neb_users_stats` (`userid`, `name`, `value`) VALUES
+(2, 'kills', 11);
+
 CREATE TABLE IF NOT EXISTS `neb_users_stats_kills` (
-  `userid` bigint(20) NOT NULL,
-  `x` int(11) NOT NULL,
-  `y` int(11) NOT NULL,
-  `z` int(11) NOT NULL,
-  `weapon` varchar(255) NOT NULL,
-  `map` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `userid` int(11) DEFAULT NULL,
+  `x` int(11) DEFAULT NULL,
+  `y` int(11) DEFAULT NULL,
+  `z` int(11) DEFAULT NULL,
+  `map` varchar(255) DEFAULT NULL,
+  `weapon` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 ALTER TABLE `neb_achievements`
@@ -117,16 +120,11 @@ ALTER TABLE `neb_updates`
 ALTER TABLE `neb_users`
  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `neb_users_stats_kills`
- ADD KEY `userid` (`userid`);
-
 
 ALTER TABLE `neb_achievements`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-ALTER TABLE `neb_mirrors`
-MODIFY `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
 ALTER TABLE `neb_users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

fbd5c40b1db0459ee77d88f3f06adee989fb5507
Orygin <louisgeuten@gmail.com>
2016-06-12 21:01:16 +0200

    Moved corresponding parts of the admin.go file to their respective files

---
+++ core/achievements.go
@@ -0,0 +1,93 @@
+package core
+
+type AchievementData struct {
+	Id       int
+	Name     string
+	Max      int
+	FullName string
+	FullDesc string
+	Icon     string
+}
+
+func GetAchievementsData() ([]AchievementData, error) {
+	var achs []AchievementData
+	rows, err := Db.Query("SELECT id, name, max, fullName, fullDesc, icon FROM neb_achievements")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var ach AchievementData
+		err := rows.Scan(&ach.Id, &ach.Name, &ach.Max, &ach.FullName, &ach.FullDesc, &ach.Icon)
+		if err != nil {
+			Error.Println("Could not get achievement table infos :", err)
+			return nil, err
+		}
+		achs = append(achs, ach)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		Error.Println("Could not get achievements tables infos :", err)
+		return nil, err
+	}
+	return achs, nil
+}
+func GetAchievementData(id string) (AchievementData, error) {
+	var ach AchievementData
+	err := Db.QueryRow("SELECT id, name, max, fullName, fullDesc, icon FROM neb_achievements WHERE id=?", id).Scan(&ach.Id, &ach.Name, &ach.Max, &ach.FullName, &ach.FullDesc, &ach.Icon)
+	if err != nil {
+		return ach, err
+	}
+	return ach, nil
+}
+
+func SetAchievementData(id int, data AchievementData) error {
+	stmt, err := Db.Prepare("UPDATE neb_achievements SET name=?, max=?, fullName=?, fullDesc=?, icon=? WHERE id=?")
+	if err != nil {
+		Error.Println("Could not prepare achievement data update :", err)
+		return err
+	}
+
+	_, err = stmt.Exec(data.Name, data.Max, data.FullName, data.FullDesc, data.Icon, data.Id)
+	if err != nil {
+		Error.Println("Could not execute achievement data update : ", err, data)
+		return err
+	}
+	return nil
+}
+
+func AddAchievementData(data AchievementData) (AchievementData, error) {
+	stmt, err := Db.Prepare("INSERT INTO neb_achievements (name, max, fullName, fullDesc, icon) VALUES (?, ?, ?, ?, ?)")
+	if err != nil {
+		Error.Println("Could not prepare achievement data update :", err)
+		return data, err
+	}
+
+	res, err := stmt.Exec(data.Name, data.Max, data.FullName, data.FullDesc, data.Icon)
+	if err != nil {
+		Error.Println("Could not execute achievement data update : ", err, data)
+		return data, err
+	}
+	id, err := res.LastInsertId()
+	data.Id = int(id)
+
+	return data, nil
+}
+
+func DeleteAchievementData(id int) error {
+	stmt, err := Db.Prepare("DELETE FROM neb_achievements WHERE id=?")
+	if err != nil {
+		Error.Println("Could not prepare achievement data deletion :", err)
+		return err
+	}
+
+	_, err = stmt.Exec(id)
+	if err != nil {
+		Error.Println("Could not execute achievement data deletion : ", err)
+		return err
+	}
+
+	return nil
+}
